ecf24c5313783787365ba14bf6b7eef8040b389c
kwojnar <karol.wojnar@mail.com>
2024-12-31 21:49:46 +0100

    added split nodes delimiter function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -24,6 +24,55 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is not random", TextType.BOLD, "urly://url.url")
         self.assertNotEqual(node, node2)
 
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_italic(self):
+        test_case = split_nodes_delimiter([TextNode("Normal *Italic* Another Normal", TextType.NORMAL)], "*", TextType.ITALIC)
+        expected_result = [
+                TextNode("Normal ", TextType.NORMAL),
+                TextNode("Italic", TextType.ITALIC),
+                TextNode(" Another Normal", TextType.NORMAL)
+                ]
+        self.assertEqual(test_case, expected_result)
+
+
+    def test_multiple_italic(self):
+        test_case = split_nodes_delimiter([TextNode("Normal *Italic* *Another Italic*", TextType.NORMAL)], "*", TextType.ITALIC)
+        expected_result = [
+                TextNode("Normal ", TextType.NORMAL),
+                TextNode("Italic", TextType.ITALIC),
+                TextNode("Another Italic", TextType.ITALIC)
+                ]
+    
+    def test_bold(self):
+        test_case = split_nodes_delimiter([TextNode("Normal **Bold** Another Normal", TextType.NORMAL)], "**", TextType.BOLD)
+        expected_result = [
+                TextNode("Normal ", TextType.NORMAL),
+                TextNode("Bold", TextType.BOLD),
+                TextNode(" Another Normal", TextType.NORMAL)
+                ]
+        self.assertEqual(test_case, expected_result)
+    
+
+    def test_code(self):
+        test_case = split_nodes_delimiter([TextNode("`Code` Normal", TextType.NORMAL)], "`", TextType.CODE)
+        expected_result = [
+                TextNode("Code", TextType.CODE),
+                TextNode(" Normal", TextType.NORMAL)
+                ]
+        self.assertEqual(test_case, expected_result)
+
+
+    def test_delim_bold_and_italic(self):
+        node = TextNode("**bold** and *italic*", TextType.NORMAL)
+        test_case = split_nodes_delimiter([node], "**", TextType.BOLD)
+        test_case = split_nodes_delimiter(test_case, "*", TextType.ITALIC)
+        expected_result = [
+                TextNode("bold", TextType.BOLD),
+                TextNode(" and ", TextType.NORMAL),
+                TextNode("italic", TextType.ITALIC),
+            ]
+        self.assertEqual(test_case, expected_result)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -25,3 +25,21 @@ class TextNode():
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.NORMAL:
+            new_nodes.append(node)
+            continue
+        split_node = node.text.split(delimiter)
+        if len(split_node) % 2 == 0:
+            raise ValueError("Invalid Markdown. formatted section not closed")
+        for index in range(len(split_node)):
+            if split_node[index] == "":
+                continue
+            if index % 2 == 0:
+                new_nodes.append(TextNode(split_node[index], TextType.NORMAL))
+            else:
+                new_nodes.append(TextNode(split_node[index], text_type))
+    return new_nodes
