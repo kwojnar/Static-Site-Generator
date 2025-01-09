02ca65425e5889ce842d19b27bfff8936536d96e
kwojnar <karol.wojnar@mail.com>
2025-01-09 22:30:52 +0100

    added markdown to html and helping functions

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,11 +1,12 @@
 import re
 
-block_type_heading = "heading"
-block_type_code = "code"
-block_type_quote = "quote"
-block_type_ulist = "unordered_list"
-block_type_olist = "ordered_list"
-block_type_paragraph = "paragraph"
+class BlockType:
+    block_type_heading = "heading"
+    block_type_code = "code"
+    block_type_quote = "quote"
+    block_type_ulist = "unordered_list"
+    block_type_olist = "ordered_list"
+    block_type_paragraph = "paragraph"
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -21,9 +22,9 @@ def block_to_block_type(markdown_block):
     ordered_list_regex = r"^\d*[.]\s*"
     ordered_list__start_regex = r"^1*[.]\s*"
     if re.search(heading_regex, markdown_block):
-        return block_type_heading
+        return BlockType.block_type_heading
     if re.search(code_regex, markdown_block):
-        return block_type_code
+        return BlockType.block_type_code
     if re.search(quote_regex, markdown_block):
         block_lines = markdown_block.split("\n")
         is_quote_block = True
@@ -31,7 +32,7 @@ def block_to_block_type(markdown_block):
             if not re.search(quote_regex, block_line):
                 is_quote_block = False
         if is_quote_block:
-            return block_type_quote
+            return BlockType.block_type_quote
     if re.search(unordered_list_regex, markdown_block):
         block_lines = markdown_block.split("\n")
         is_unordered_list_block = True
@@ -39,7 +40,7 @@ def block_to_block_type(markdown_block):
             if not re.search(unordered_list_regex, block_line):
                 is_unordered_list_block = False
         if is_unordered_list_block:
-            return block_type_ulist
+            return BlockType.block_type_ulist
     if re.search(ordered_list__start_regex, markdown_block):
         block_lines = markdown_block.split("\n")
         is_ordered_list_block = True
@@ -49,6 +50,6 @@ def block_to_block_type(markdown_block):
                 is_ordered_list_block = False
             next_index += 1
         if is_ordered_list_block:
-            return block_type_olist
-    return block_type_paragraph
+            return BlockType.block_type_olist
+    return BlockType.block_type_paragraph
     
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
-from textnode import TextNode, TextType
-from htmlnode import LeafNode
+from textnode import *
+from htmlnode import *
+from block_markdown import *
+import re
 
 
 def main():
@@ -26,6 +28,50 @@ def text_node_to_html_node(text_node):
             return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception("Incorrect Text Type")
+        
+def text_to_html_nodes(text):
+    text_nodes = text_to_textnodes(text)
+    html_nodes = list(map(text_node_to_html_node, text_nodes))
+    return html_nodes
+
+def markdown_block_to_html_node(markdown_block, markdown_block_type):
+    match markdown_block_type:
+        case BlockType.block_type_heading:
+            count = re.match(r"(^#)", markdown_block).end()
+            html_nodes = text_to_html_nodes(markdown_block[count:])
+            return ParentNode(tag=f"h{count}", children=html_nodes)
+        case BlockType.block_type_code:
+            html_nodes = text_to_html_nodes(markdown_block.strip("```"))
+            return ParentNode(tag="code", children=html_nodes)
+        case BlockType.block_type_quote:
+            quote_blocks = markdown_block.split("\n")
+            quote_markdown_block = "\n".join(map(lambda line: line.lstrip("> "), quote_blocks))
+            html_nodes = text_to_html_nodes(quote_markdown_block)
+            return ParentNode(tag="quote", children=html_nodes)
+        case BlockType.block_type_ulist:
+            ulist_blocks = markdown_block.split("\n")
+            ulist_markdown_block = "\n".join(map(lambda line: line[2:], ulist_blocks))
+            html_nodes = text_to_html_nodes(ulist_markdown_block)
+            return ParentNode(tag="ul", children=html_nodes)
+        case BlockType.block_type_olist:
+            olist_blocks = markdown_block.split("\n")
+            olist_markdown_block = "\n".join(map(lambda line: re.match(r"(?<=^\d*[.]\s)(.*)", line).group(0), olist_blocks))
+            html_nodes = text_to_html_nodes(olist_markdown_block)
+            return ParentNode(tag="ol", children=html_nodes)
+        case BlockType.block_type_paragraph:
+            html_nodes = text_to_html_nodes(markdown_block)
+            return ParentNode(tag="p", children=html_nodes)
+        case _:
+            pass
+
+def makrdown_to_html_node(markdown):
+    markdown_blocks = markdown_to_blocks(markdown)
+    children_nodes = []
+    for markdown_block in markdown_blocks:
+        markdown_block_type = block_to_block_type(markdown_block)
+        children_nodes.append(markdown_block_to_html_node(markdown_block, markdown_block_type))
+    return ParentNode(tag="div", children=children_nodes)
+
 
 if __name__ == "__main__":
     main()
