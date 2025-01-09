51d01ebf5aedc336a83c3fd293cf6afb3fe519e5
kwojnar <karol.wojnar@mail.com>
2025-01-09 21:28:01 +0100

    added block_type variables

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,12 @@
 import re
 
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_ulist = "unordered_list"
+block_type_olist = "ordered_list"
+block_type_paragraph = "paragraph"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     blocks = list(map(lambda block: block.strip(), blocks))
@@ -14,9 +21,9 @@ def block_to_block_type(markdown_block):
     ordered_list_regex = r"^\d*[.]\s*"
     ordered_list__start_regex = r"^1*[.]\s*"
     if re.search(heading_regex, markdown_block):
-        return "heading"
+        return block_type_heading
     if re.search(code_regex, markdown_block):
-        return "code"
+        return block_type_code
     if re.search(quote_regex, markdown_block):
         block_lines = markdown_block.split("\n")
         is_quote_block = True
@@ -24,7 +31,7 @@ def block_to_block_type(markdown_block):
             if not re.search(quote_regex, block_line):
                 is_quote_block = False
         if is_quote_block:
-            return "quote"
+            return block_type_quote
     if re.search(unordered_list_regex, markdown_block):
         block_lines = markdown_block.split("\n")
         is_unordered_list_block = True
@@ -32,7 +39,7 @@ def block_to_block_type(markdown_block):
             if not re.search(unordered_list_regex, block_line):
                 is_unordered_list_block = False
         if is_unordered_list_block:
-            return "unordered_list"
+            return block_type_ulist
     if re.search(ordered_list__start_regex, markdown_block):
         block_lines = markdown_block.split("\n")
         is_ordered_list_block = True
@@ -42,6 +49,6 @@ def block_to_block_type(markdown_block):
                 is_ordered_list_block = False
             next_index += 1
         if is_ordered_list_block:
-            return "ordered_list"
-    return "paragraph"
+            return block_type_olist
+    return block_type_paragraph
     
